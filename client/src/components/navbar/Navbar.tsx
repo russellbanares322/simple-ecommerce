9e59d5fa0aeb41e4d5fafa9f60851bcae696bd66
russellbanares322 <banaresrussell11@gmail.com>
2023-08-06 11:52:41 +0800

    feat: hide nav links in small screen device

--- client/src/components/navbar/Navbar.tsx
+++ client/src/components/navbar/Navbar.tsx
@@ -1,33 +1,48 @@
 import logo from "../../assets/logo.png";
-import { HiMenu } from "react-icons/hi";
+import {
+  HiMenu,
+  HiOutlineSearch,
+  HiOutlineUser,
+  HiOutlineShoppingCart,
+} from "react-icons/hi";
+
 function Navbar() {
   return (
-    <navbar className="flex justify-between items-center w-full gap-3">
+    <nav className="flex justify-between items-center w-full gap-2">
       <div className="flex justify-between items-center">
         <div className="flex justify-center items-center gap-2">
           <img className="h-12 w-14 object-contain" src={logo} alt="logo" />
-          <h1 className="font-bold text-lg">Shopcart</h1>
+          <h1 className="font-bold text-xl text-soft-green cursor-pointer">
+            Shopcart
+          </h1>
         </div>
-        <HiMenu className="text-xl hidden" />
+        <HiMenu className="text-xl visible md:hidden" />
       </div>
-      <ul className="md:flex md:justify-center md:items-center gap-5 ml-5 text-sm font-medium">
-        <li>Categories</li>
-        <li>Deals</li>
-        <li>What's New</li>
-        <li>Delivery</li>
+      <ul className="hidden md:flex md:justify-center md:items-center gap-7 ml-5 text-sm font-medium">
+        <li className="cursor-pointer">Categories</li>
+        <li className="cursor-pointer">Deals</li>
+        <li className="cursor-pointer">What's New</li>
+        <li className="cursor-pointer">Delivery</li>
       </ul>
-      <div className="relative">
+      <div className="relative hidden md:block">
         <input
           className="bg-gray px-5 py-2 rounded-full outline-none text-sm w-72 font-medium"
           placeholder="Search product"
           type="text"
         />
+        <HiOutlineSearch className="text-lg absolute top-[0.6rem] right-5" />
       </div>
-      <div className="flex justify-center items-center gap-5 text-sm font-medium">
-        <p>Account</p>
-        <p>Cart</p>
+      <div className="justify-center items-center gap-5 text-sm font-medium hidden md:flex">
+        <p className="flex gap-1 cursor-pointer">
+          <HiOutlineUser className="text-lg" />
+          Account
+        </p>
+        <p className="flex gap-1 cursor-pointer">
+          <HiOutlineShoppingCart className="text-lg" />
+          Cart
+        </p>
       </div>
-    </navbar>
+    </nav>
   );
 }
 
