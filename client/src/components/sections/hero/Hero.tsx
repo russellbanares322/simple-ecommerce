c8fbf5cac4e165d6b8ef9a889cd2a548d27bc372
russellbanares322 <banaresrussell11@gmail.com>
2023-08-08 22:19:52 +0800

    feat: adjust breakpoint in hero component

--- client/src/components/sections/hero/Hero.tsx
+++ client/src/components/sections/hero/Hero.tsx
@@ -4,11 +4,11 @@ import CreditCard from "../../../assets/CreditCard";
 const Hero: React.FC = () => {
   return (
     <div className="page-padding-y page-padding-x bg-hero-bg bg-cover bg-center relative overflow-hidden">
-      <div className="my-14 lg:my-28">
-        <p className="font-semibold text-soft-green text-[2rem] w-[20rem] lg:text-[4rem] lg:w-[30rem]">
+      <div className="my-14 md:my-28">
+        <p className="font-semibold text-soft-green text-[2rem] w-[20rem] md:text-[4rem] md:w-[30rem]">
           Shopping And Department Store.
         </p>
-        <p className="my-5 lg:my-5 w-[20rem] lg:w-[27rem] text-md lg:text-[1.1rem]">
+        <p className="my-5 md:my-5 w-[20rem] md:w-[27rem] text-md md:text-[1.1rem]">
           Shopping is a bit of relaxing hobby for me, which is sometimes
           troubling for the bank balance.
         </p>
