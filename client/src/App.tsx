2d39a7ca0cd4090f1e9c12569df6a0b47e9cd7e5
russellbanares322 <banaresrussell11@gmail.com>
2023-08-24 19:48:23 +0800

    feat: install react-query library for api fetching

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,4 @@
+import { QueryClient, QueryClientProvider } from "react-query";
 import { Route, Routes } from "react-router-dom";
 import Footer from "./components/footer/Footer";
 import Navbar from "./components/navbar/Navbar";
@@ -6,17 +7,21 @@ import DealsPage from "./pages/deals/DealsPage";
 import Home from "./pages/home/Home";
 
 function App() {
+  const queryClient = new QueryClient();
+
   return (
-    <div className="font-poppins min-h-screen h-full relative">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/deals" element={<DealsPage />} />
-        <Route path="/login" element={<AuthLayout />} />
-        <Route path="/sign-up" element={<AuthLayout />} />
-      </Routes>
-      <Footer />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="font-poppins min-h-screen h-full relative">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/deals" element={<DealsPage />} />
+          <Route path="/login" element={<AuthLayout />} />
+          <Route path="/sign-up" element={<AuthLayout />} />
+        </Routes>
+        <Footer />
+      </div>
+    </QueryClientProvider>
   );
 }
 
