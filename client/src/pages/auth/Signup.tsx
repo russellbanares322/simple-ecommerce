896765109e534815445f8caa82a05a69c7f7c288
russellbanares322 <banaresrussell11@gmail.com>
2023-09-14 20:59:38 +0800

    feat: implement showing of password in signup page

--- client/src/pages/auth/Signup.tsx
+++ client/src/pages/auth/Signup.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import inputWarningMessage from "./inputWarningMessage";
 import { TSignUpFormDataTypes } from "./types";
 import { register } from "../../api/authenticationApi";
+import { HiOutlineEyeOff, HiOutlineEye } from "react-icons/hi";
 import toast from "react-hot-toast";
 
 const Signup: React.FC = () => {
@@ -14,6 +15,8 @@ const Signup: React.FC = () => {
     confirmPassword: "",
   });
   const [isFormDirty, setIsFormDirty] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [showPassword, setShowPassword] = useState<boolean>(false);
   const isFormInputsEmpty =
     !formData.username ||
     !formData.email ||
@@ -41,9 +44,14 @@ const Signup: React.FC = () => {
     }
   };
 
+  const handleTogglePasswordDisplay = () => {
+    setShowPassword(!showPassword);
+  };
+
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     handleValidateInputs();
+    setIsLoading(true);
     if (!isFormInputsEmpty) {
       register(formData.username, formData.email, formData.password)
         .then(() => {
@@ -51,9 +59,11 @@ const Signup: React.FC = () => {
           toast.success(
             "Successfully created account, please login to continue."
           );
+          setIsLoading(false);
         })
         .catch(() => {
           toast.error("Failed to create account, please try again.");
+          setIsLoading(false);
         });
     }
   };
@@ -96,13 +106,31 @@ const Signup: React.FC = () => {
         <label className="text-sm font-medium" htmlFor="password">
           Password
         </label>
-        <input
-          value={formData.password}
-          onChange={handleFormDataChange}
-          name="password"
-          className="input-style"
-          type="Password"
-        />
+        <div className="relative">
+          <input
+            onChange={handleFormDataChange}
+            value={formData.password}
+            name="password"
+            className="input-style w-full"
+            type={showPassword ? "text" : "password"}
+          />
+          {formData.password.length > 0 && showPassword && (
+            <HiOutlineEye
+              onClick={handleTogglePasswordDisplay}
+              className="absolute top-4 right-2 cursor-pointer"
+              title="Show Password"
+              size={21}
+            />
+          )}
+          {formData.password.length > 0 && !showPassword && (
+            <HiOutlineEyeOff
+              className="absolute top-4 right-2 cursor-pointer"
+              onClick={handleTogglePasswordDisplay}
+              title="Hide Password"
+              size={21}
+            />
+          )}
+        </div>
         {isFormDirty && !formData.password && inputWarningMessage("Password")}
         <label className="text-sm font-medium" htmlFor="confirm-password">
           Confirm Password
@@ -112,7 +140,7 @@ const Signup: React.FC = () => {
           onChange={handleFormDataChange}
           name="confirmPassword"
           className="input-style"
-          type="Password"
+          type={showPassword ? "text" : "password"}
         />
         {passwordsMismatch && (
           <p className="text-red-600 my-1 text-xs">Passwords doesn't match</p>
@@ -120,8 +148,13 @@ const Signup: React.FC = () => {
         {isFormDirty &&
           !formData.confirmPassword &&
           inputWarningMessage("Confirm Password")}
-        <button type="submit" className="button-flat-filled-style mt-3">
-          SIGNUP
+
+        <button
+          disabled={isLoading}
+          type="submit"
+          className="button-flat-filled-style mt-3"
+        >
+          {isLoading ? "SIGNING UP..." : "SIGNUP"}
         </button>
       </form>
     </div>
