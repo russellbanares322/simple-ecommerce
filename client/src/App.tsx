76847bbe69314263e6556c9d06eff71b304a9b01
russellbanares322 <banaresrussell11@gmail.com>
2023-12-21 20:31:47 +0800

    feat: move QueryClientProvider in main.tsx component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,3 @@
-import { QueryClient, QueryClientProvider } from "react-query";
-import { ReactQueryDevtools } from "react-query/devtools";
 import { Route, Routes } from "react-router-dom";
 import Footer from "./components/footer/Footer";
 import Navbar from "./components/navbar/Navbar";
@@ -13,38 +11,32 @@ import Home from "./pages/home/Home";
 import ProductDetails from "./pages/product/ProductDetails";
 
 function App() {
-  const queryClient = new QueryClient();
   const { isAuthenticated } = useAuthentication();
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <div className="font-poppins min-h-screen h-full relative">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          {/* TODO: Make the product details a child of DealsPage and render it using Outlet  */}
-          <Route path="/deals" element={<DealsPage />} />
-          <Route path="/deals/:productId" element={<ProductDetails />} />
-          {!isAuthenticated && <Route path="/login" element={<AuthLayout />} />}
-          {!isAuthenticated && (
-            <Route path="/sign-up" element={<AuthLayout />} />
-          )}
-          {isAuthenticated && (
-            <Route
-              path="/my-account"
-              element={
-                <ProtectedRoute isAuthenticated={isAuthenticated}>
-                  <Account />
-                </ProtectedRoute>
-              }
-            />
-          )}
-          <Route path="*" element={<ErrorPage />} />
-        </Routes>
-        <Footer />
-      </div>
-      <ReactQueryDevtools />
-    </QueryClientProvider>
+    <div className="font-poppins min-h-screen h-full relative">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* TODO: Make the product details a child of DealsPage and render it using Outlet  */}
+        <Route path="/deals" element={<DealsPage />} />
+        <Route path="/deals/:productId" element={<ProductDetails />} />
+        {!isAuthenticated && <Route path="/login" element={<AuthLayout />} />}
+        {!isAuthenticated && <Route path="/sign-up" element={<AuthLayout />} />}
+        {isAuthenticated && (
+          <Route
+            path="/my-account"
+            element={
+              <ProtectedRoute isAuthenticated={isAuthenticated}>
+                <Account />
+              </ProtectedRoute>
+            }
+          />
+        )}
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+      <Footer />
+    </div>
   );
 }
 
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,20 +6,27 @@ import "./index.css";
 import "react-loading-skeleton/dist/skeleton.css";
 import { SkeletonTheme } from "react-loading-skeleton";
 import { Toaster } from "react-hot-toast";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <SkeletonTheme baseColor="#D3D3D3" highlightColor="#44444444">
-        <App />
-        <Toaster
-          position="top-center"
-          reverseOrder={false}
-          gutter={8}
-          toastOptions={{
-            duration: 5000,
-          }}
-        />
+        <QueryClientProvider client={queryClient}>
+          <App />
+          <Toaster
+            position="top-center"
+            reverseOrder={false}
+            gutter={8}
+            toastOptions={{
+              duration: 5000,
+            }}
+          />
+          <ReactQueryDevtools />
+        </QueryClientProvider>
       </SkeletonTheme>
     </BrowserRouter>
   </React.StrictMode>
